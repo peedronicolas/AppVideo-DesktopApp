8ee0f828c828b344819a1238d1a31a85b8f7f6cb
peedronicolas <pedro.nicolas62@gmail.com>
2022-02-13 18:30:29 +0100

    Commit inicial. Se crea el proyecto Maven para comenzar a trabajar.

---
+++ appvideo/src/main/java/um/tds/appvideo/App.java
@@ -0,0 +1,13 @@
+package um.tds.appvideo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
